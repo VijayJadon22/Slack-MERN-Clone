918ce4e8e65294bfc586dfc1722c3eca8905f97e
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-08-27 23:47:23 +0530

    Called the connectToDB function in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./config/env.js";
+import { connectToDB } from "./config/db.js";
 
 const app = express();
 
@@ -10,4 +11,5 @@ app.get("/", (req, res) => {
 
 app.listen(ENV.PORT, () => {
     console.log(`Server is running at PORT: ${ENV.PORT}`);
+    connectToDB();
 })
