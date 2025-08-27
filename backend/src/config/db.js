103a44569178ab37a862c2a94dfdcb072b3acc0b
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-08-27 23:44:23 +0530

    created function in config to connect to MongoDB

---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+import { ENV } from "./env";
+
+export const connectToDB = async () => {
+    try {
+        const conn = await mongoose.connect(ENV.MONGO_URI);
+        console.log(`Connected To DB: ${conn.connection.host}`);
+    } catch (error) {
+        console.log(`Error connecting to MONGODB: ${error}`);
+        process.exit(1);
+    }
+}
