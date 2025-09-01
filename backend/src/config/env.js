fed13fae9914383947777b476974023fbcbd6351
Vijay Jadon <vijaypratapsinghjadon22@gmail.com>
2025-09-01 23:18:48 +0530

    Added Stream secret and api key for video messaging and calls

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -6,5 +6,7 @@ export const ENV = {
     MONGO_URI: process.env.MONGO_URI,
     NODE_ENV: process.env.NODE_ENV,
     CLERK_PUBLISHABLE_KEY: process.env.CLERK_PUBLISHABLE_KEY,
-    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY
+    CLERK_SECRET_KEY: process.env.CLERK_SECRET_KEY,
+    STREAM_API_KEY: processe.env.STREAM_API_KEY,
+    STREAM_API_SECRET: process.env.STREAM_API_SECRET
 }
